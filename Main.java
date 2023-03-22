9c3e7bf7ad095c9fd050d1a2e43f0281db821f00
Zakhar Melnikov <melnikoff.zakhar@ya.ru>
2023-03-22 22:51:01 +0300

    first commit

--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
-        double a = -5;
-        double b = 5;
-        double x = a;
-        double y = x*x*x + x +1;
-        double square = 0;
-        double delta = 0.02;
-        while(x < b)
-        {
-            square += delta*(y + x*x*x + x +1)/2;
-            y = x*x*x + x +1;
-            x += delta;
+        //Задание 4
+        class Creator{
+            public void create(String[] typeOfDeveloper) {
+                switch (typeOfDeveloper[0]) {
+                    case "JavaDeveloper" :
+                        JavaDeveloper javaDeveloper = new JavaDeveloper(typeOfDeveloper[1], typeOfDeveloper[2], Integer.parseInt(typeOfDeveloper[3]), typeOfDeveloper[4]);
+                        javaDeveloper.doCode();
+                        break;
+                    case "PythonDeveloper" :
+                        PythonDeveloper pythonDeveloper= new PythonDeveloper(typeOfDeveloper[1], typeOfDeveloper[2], Integer.parseInt(typeOfDeveloper[3]), typeOfDeveloper[4]);
+                        pythonDeveloper.doCode();
+                        break;
+                    default :
+                        throw new RuntimeException("error: no such type of developer");
+                }
+            }
         }
-        System.out.println(square);
+        String[] developer1java = {"JavaDeveloper","Xprt1","juniour","1","Pattern, SQL, some technologies"};
+        String[] developer2java = {"JavaDeveloper","Xprt2","juniour","2","Computer science, SQL, class"};
+        String[] developer1python = {"PythonDeveloper","Xprt3","middle","15","SQL, HTML"};
+        String[] developer2python = {"PythonDeveloper","Xprt4","juniour","2","Flask, Cherry, Django"};
+        Creator creator = new Creator();
+        creator.create(developer1java);
+        creator.create(developer2java);
+        creator.create(developer1python);
+        creator.create(developer2python);
+
     }
 }
